6e214f5082e858cbd3884b8f2a599c4d66236fcc
nishanttdhiman <nishantdhiman0209@gmail.com>
2025-06-06 08:28:58 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,14 @@
+let count=document.querySelector("#counter");
+
+const increment=()=>{
+    let value=parseInt(count.innerText);
+    value=value+1;
+    count.innerText=value;
+}
+
+
+const decrement=()=>{
+    let value=parseInt(count.innerText);
+    value=value-1;
+    count.innerText=value;
+}
